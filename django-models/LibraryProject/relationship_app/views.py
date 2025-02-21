ec001ae52372185262090cba32c09dee888afa82
okinyoCoder <okinyoanthony@gmail.com>
2025-02-21 15:01:13 +0300

    . Django Views and URL Configuration debug version 1.6

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -10,10 +10,10 @@ from django.views.generic.detail import DetailView
 def book_list(request):
     books = Book.objects.all()
     context = {'book_list': books}
-    return render(request, 'templates/list_books.html', context)
+    return render(request, 'relationship_app/list_books.html', context)
 
 #class-based view that displays details for a specific library, 
 # listing all books available in that library.
 class LibraryDetailView(DetailView):
-    template_name = 'templates/library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     model = Library
