f89a4ac9a31a2ed1b70ba5f3a6e4a8699242f1df
okinyoCoder <okinyoanthony@gmail.com>
2025-02-23 18:35:53 +0300

    Implementing User Authentication in Django debug 1.0

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,7 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from django.contrib.auth import login
 
 # Create your views here.
 
