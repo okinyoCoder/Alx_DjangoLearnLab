45aa4a5d26d0375d48f451196c408cdfd04b8740
okinyoCoder <okinyoanthony@gmail.com>
2025-02-23 19:00:12 +0300

    Implementing User Authentication in Django debug 1.0

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from .views import list_books
 from .views import LibraryDetailView
-from .views import Registration
+from .views import Registration, RelationLoginView,RelationLogoutView
 from . import views
 
 urlpatterns = [
     path('book/', views.book_lists, name='book'),
     path('library/', views.LibraryDetailView.as_view(), name='library'),
     path('register/', views.Registration.as_view(), name='register'),
-    path("relationship_app/", include("django.contrib.auth.urls")),
+    path("login/", RelationLoginView.as_view(), name="login"),
+    path("logout/", RelationLogoutView.as_view(), name="logout"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Book
 from .models import Library
+from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
@@ -22,8 +23,15 @@ class LibraryDetailView(DetailView):
     model = Library
     context_object_name = 'library'
 
-#Utilize Django’s built-in views and forms for handling user login, logout, and registration.
+#Utilize Django’s built-in views and forms for handling user registration.
 class Registration(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
+
+#Utilize Django’s built-in views and forms for handling user login
+class RelationLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+#Utilize Django’s built-in views and forms for handling user logout
+class RelationLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
