7cec02a90da424dd225de91a279d35d4d1b09f3a
okinyoCoder <okinyoanthony@gmail.com>
2025-03-01 19:28:52 +0300

    Implementing HTTPS and Secure Redirects in Django

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -133,3 +133,27 @@ X_FRAME_OPTIONS = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
+
+#Set to True to redirect all non-HTTPS requests to HTTPS
+SECURE_SSL_REDIRECT = True
+#Set an appropriate value (e.g., 31536000 for one year) to instruct browsers to only access the site via HTTPS for the specified time
+SECURE_HSTS_SECONDS = 31536000
+
+#Set to True to include all subdomains in the HSTS policy and to allow preloading
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+#Set to True to ensure session cookies are only transmitted over HTTPS
+SESSION_COOKIE_SECURE = True
+#Set to True to ensure CSRF cookies are only transmitted over HTTPS
+CSRF_COOKIE_SECURE = True
+
+# Implement Secure Headers
+#Set to “DENY” to prevent your site from being framed and protect against clickjacking
+X_FRAME_OPTIONS = "DENY"
+#Set to True to prevent browsers from MIME-sniffing a response away from the declared content-type
+SECURE_CONTENT_TYPE_NOSNIFF = True
+#Set to True to enable the browser’s XSS filtering and help prevent cross-site scripting attacks.
+SECURE_BROWSER_XSS_FILTER = True
+
+#Step 4: Update Deployment Configuration
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
