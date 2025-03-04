e1a2e53e4287e292b848aeccad362189aae74dd9
okinyoCoder <okinyoanthony@gmail.com>
2025-03-04 22:13:35 +0300

    First API Endpoint with Django REST Framework version 1.0

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -6,6 +6,6 @@ from .models import Book
 def index(request): 
     return HttpResponse('Welcome to my book store.')
 
-class BookList(generics.ListCreateAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = 'BookSerializer'
