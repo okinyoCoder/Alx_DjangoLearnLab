93687c19e5e147b360472d68cc05da5872f54848
okinyoCoder <okinyoanthony@gmail.com>
2025-03-04 21:59:45 +0300

    First API Endpoint with Django REST Framework

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import BookList
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('books/', BookList.as_view(), name='book-list'),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render, HttpResponse
+from rest_framework import generics
+from .serializers import BookSerializer
+from .models import Book
 
 def index(request): 
     return HttpResponse('Welcome to my book store.')
+
+class BookList(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = 'BookSerializer'
