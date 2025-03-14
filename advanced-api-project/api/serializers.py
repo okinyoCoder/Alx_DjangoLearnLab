3795b58d61312cce767041a30794afdc81b369f3
okinyoCoder <okinyoanthony@gmail.com>
2025-03-14 18:50:03 +0300

    new debug on validate_publication_year method

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -7,6 +7,15 @@ class BookSerializer(serializers.ModelSerializer):
         model = Book
         fields = '__all__'
 
+    def validate_publication_year(self, data):
+        """custom validation to the BookSerializer to 
+           ensure the publication_year is not in the future
+        """
+        thisYear = datetime.datetime.now().year
+        if data['publication_year'] > thisYear:
+            raise serializers.ValidationError("ensure the publication_year is not in the future")
+        return data
+
 class AuthorSerializer(serializers.ModelSerializer):
     books = BookSerializer(many=True, readonly=True)
 
