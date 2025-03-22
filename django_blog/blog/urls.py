b1d38eb26e2b8e67e43bdd2b24ed57d342f1ef96
okinyoCoder <okinyoanthony@gmail.com>
2025-03-22 13:22:49 +0300

    . Creating Blog Post Management Features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,13 +2,19 @@ from django.urls import path, include
 from .views import SignUpView
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView, LogoutView
+from .views import *
 
 urlpatterns = [
     path('',  include('django.contrib.auth.urls')),
     path('login/', LoginView.as_view(template_name='blog/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
     path('profile/',
              TemplateView.as_view(template_name='blog/profile.html'),
              name='profile'),
     path("register/", SignUpView.as_view(), name="blog/register"),
+    path('/posts/new/', PostCreateView.as_view(), name='post-create'),
+    path('/posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('/posts/', PostListView.as_view(), name='post-list'),
+    path('/posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-edit'),
+    path('/posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from .models import Post
+from .form import CreatePostForm
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 # Authentication Views create User
@@ -30,3 +33,41 @@ def edit_profile(request, user_id):
     else:
         form = UserCreationForm(instance=obj)
     return render(request, 'blog/register.html', {'form': form})
+
+# CRUD View Setup
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/list_post.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    template_name = 'blog/post.html'
+    form_class = CreatePostForm
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else: return False
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/post_delete.html'
+    success_url=reverse_lazy('post-list')
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'blog/post.html'
+    form_class = CreatePostForm
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
