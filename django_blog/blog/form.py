0b8ecaf6a260fd3a1ccace693d3159404edc5cc9
okinyoCoder <okinyoanthony@gmail.com>
2025-03-27 19:51:43 +0300

    Adding Comment Functionality to Blog Posts v1

--- django_blog/blog/form.py
+++ django_blog/blog/form.py
@@ -9,4 +9,13 @@ class CreatePostForm(forms.ModelForm):
 class CommentForm(forms.ModelForm):
     class meta:
         model = Comment
-        fields = "__all__"
+        fields = ['content',]
+
+    def validate_context(self):
+        """custom validation to the BookSerializer to 
+           ensure the publication_year is not in the future
+        """
+        content = self.cleaned_data.get("content")
+        if not content or len(content) < 5:
+            raise forms.ValidationError("Comment must be at least 5 characters long")
+        return content
