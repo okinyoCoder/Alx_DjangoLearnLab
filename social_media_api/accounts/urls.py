71c42d7acf37d1ca9b79c61e8d7339ebcdf3e341
okinyoCoder <okinyoanthony@gmail.com>
2025-03-29 20:30:10 +0300

    Project Setup and User Authentication for a Social Media API debug v1.1

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,8 +1,9 @@
-from .views import register, profile, loginView
+from .views import register, profile, loginView, CustomAuthToken
 from django.urls import path
 
 urlpatterns = [
     path('register/', register, name='user-register'),
     path('login/', loginView, name='user-login'),
     path('profile/', profile, name='user-profilr'),
+    path('api-token-auth/', CustomAuthToken.as_view())
 ]
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -7,6 +7,7 @@ from rest_framework import status
 from .models import CustomUser
 from django.contrib.auth import authenticate, login
 from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
 
 @api_view(["POST"])
 def register(request):
@@ -37,6 +38,20 @@ def loginView(request):
             user = authenticate(request, email=email, password=password)
             
             if user is not None:
+                Token.objects.get_or_create(user=user)
                 login(request, user)
                 return Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
         return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+    
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
